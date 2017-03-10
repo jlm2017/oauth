bdfd0e8638cd0dd54d8ba1f9569e6cb648d9149a
Arthur Cheysson <arthur@cheysson.fr>
2017-03-10 13:47:44 +0100

    Allow clients to add query parameters to their redirect urls

--- models/client.js
+++ models/client.js
@@ -1,7 +1,8 @@
-const scopes = require('./scopes.json');
 const bcrypt = require('bcrypt');
+const url = require('url');
 
 const {Clients} = require('../io/api');
+const scopes = require('./scopes.json');
 
 // used against timing attacks
 const fakeHash = '$2a$10$hMzUK.S/9LM5XLuPyuc6d.5xpTPODX/Ke4kprEB12/XLm7b6be7jy';
@@ -11,14 +12,23 @@ const scopes_map = scopes.reduce((obj, scope) => {
   return obj;
 }, {});
 
+function removeQueryPart(uri) {
+  const {protocol, host, pathname} = url.parse(uri);
+
+  return `${protocol}//${host}${pathname}`;
+}
+
 exports.find = function find(id) {
   return Clients.get(id);
 };
 
+
 exports.findAndValidateClient = function findAndValidateClient(id, redirectURI, scopes) {
+  const baseURI = removeQueryPart(redirectURI);
+
   return Clients.get(id)
     .then((client) => {
-      const valid = client && client.uris.includes(redirectURI) && scopes.every((scope) => client.scopes.includes(scope));
+      const valid = client && client.uris.includes(baseURI) && scopes.every((scope) => client.scopes.includes(scope));
       return valid ? client : null;
     });
 };
